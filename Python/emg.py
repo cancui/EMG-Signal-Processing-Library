910ce6aa5acfb5a5c06417c6837792854599e64c
Can Cui <can.cui888@gmail.com>
2016-06-18 08:57:40 -0400

    Refactored EMG library

--- Python/emg.py
+++ Python/emg.py
@@ -1,14 +1,97 @@
+#Make classes for movingAvg 
+
+#Encapsulation of a key function that some filters require
+class Moving_Average(object):
+    def __init__(self, length, return_int = False):
+        self.data = []
+        self.data_sum = -1
+        self.data_avg = -1
+        self.length = length
+        self.value = -1
+        self.return_int = return_int
+
+    def get_movingAvg (self, data):
+        self.data.insert(0, data)
+        self.data_sum += data
+
+        if len(self.data) > self.length:
+            self.data_sum -= self.data.pop()
+
+        if self.return_int == True:
+            self.value = int(self.data_sum / self.length) #preserves integer form
+        else: 
+            self.value = 1.0 * self.data_sum / self.length
+
+        if len(self.data) < (self.length / 2):
+            return -1
+        else:
+            return self.value
+
+#Simplifies the creation of a moving average-based LPF 
+class LPF(Moving_Average):
+    def __init__(self, cutoff_frequency, sample_frequency, return_int = False):
+        length = int(0.125 * sample_frequency / cutoff_frequency)
+        Moving_Average.__init__(self, length, return_int = return_int)
+
+    def filter(self, to_filter):
+        if self.length < 2:
+            return to_filter
+        else:
+            return self.get_movingAvg(to_filter) 
+
+class PkPk(object):
+    def __init__(self, sample_frequency, min_frequency, max_frequency):
+        self.for_pkpk = []
+        self.min_frequency = min_frequency
+        self.max_frequency = max_frequency
+        self.min_pk_gap = sample_frequency / max_frequency
+        self.max_pk_gap = sample_frequency / min_frequency
+
+        #self.pk_indices = [] 
+        #self.pk_is_convex = []
+
+    def get_pkpk(self, data):
+        self.for_pkpk.insert(0, data)
+
+        #discards any data beyond two periods of the lowest-frequency wave
+        if len(self.for_pkpk) > (self.max_pk_gap * 2): 
+            self.for_pkpk.pop()
+
+        highest = max(self.for_pkpk)
+        lowest = min(self.for_pkpk)
+        self.neutral = (highest + lowest)/2
+
+        to_return = {'max' : highest, 'min' : lowest, 'pkpk' : highest - lowest, 'neutral' : self.neutral}
+
+        if len(self.for_pkpk) < self.min_pk_gap * 2:
+            return {'max' : -1, 'min' : -1, 'pkpk' : -1, 'neutral' : -1}
+        else:
+            return to_return
+
+    def find_peaks(self):
+        #cannot find another peak of oposite concavity until min_pk_gap data points past? 
+        pass
+
+    def advanced_get_pkpk(self):
+        pass
+
 #Simple filter class assumes no low-frequency noise and data centred at 0. More complex filteres inherit from this.
 class EMG_filter_basic(object):
 
-    def __init__(self, sample_frequency = 200, range_ = 0.5):
-        self.sample_frequency = sample_frequency               #in Hz
-        self.range_ = range_                                   #in seconds
-        self.scope = 1.0 * self.sample_frequency * self.range_       #in number of samples, limits the length of movingAvg    
-        self.sum_movingAvg = 0                                 #tracks the sum of the moving average
-        self.val_movingAvg = -1                                #the latest moving average value
-        self.movingAvg = []                                    #used to store the datapoints for taking a moving average
+    def __init__(self, sample_frequency = 200, range_ = 0.1, reference_available = False):
+        #self.sample_frequency = sample_frequency               #in Hz
+        #self.range_ = range_                                   #in seconds
+        #self.scope = 1.0 * self.sample_frequency * self.range_       #in number of samples, limits the length of movingAvg    
+        #self.sum_movingAvg = 0                                 #tracks the sum of the moving average
+        #self.val_movingAvg = -1                                #the latest moving average value
+        #self.movingAvg = []                                    #used to store the datapoints for taking a moving average
         self.log = []                                          #used to store filtered data
+        self.reference_available = reference_available
+        self.MA = Moving_Average(length = sample_frequency * range_, return_int = True)
+
+        if reference_available == True:
+            self.LPF_data = LPF(cutoff_frequency = 150, sample_frequency = sample_frequency)
+            self.LPF_reference = LPF(cutoff_frequency = 150, sample_frequency = sample_frequency)
 
     #input filtered data
     def log_data(self, to_log):
@@ -30,6 +113,7 @@ class EMG_filter_basic(object):
         return abs(to_rectify)
 
     #takes the moving average within scope variable, or all of the data if that is less than scope
+    '''
     def get_movingAvg(self, data):
         self.movingAvg.insert(0, data)
         self.sum_movingAvg += data
@@ -43,13 +127,17 @@ class EMG_filter_basic(object):
             return -1
         else:
             return self.val_movingAvg
-
+    '''
     #this function is called to input raw data. It returns a filtered value if it has enough samples, and also logs it
     def filter(self, data):
-        filtered_value = self.get_movingAvg(self.rectify(data))
-        self.log_data(filtered_value)
+        if not self.reference_available:
+            filtered_value = self.MA.get_movingAvg(self.rectify(data))
+            #filtered_value = self.get_movingAvg(self.rectify(data))
+            self.log_data(filtered_value)
+            return filtered_value
 
-        return filtered_value
+        else:
+            print "Using a reference hasn't yet been implemented"
 
     def test_print(self):
         print "TEST PRINT"
@@ -57,20 +145,24 @@ class EMG_filter_basic(object):
 #extension of EMG_filter_basic that has tools to remove low-frequency noise and normalize the data
 class EMG_filter(EMG_filter_basic):
 
-    def __init__(self, sample_frequency = 200, range_ = 0.5, min_EMG_frequency = 25, max_EMG_frequency = 150):
-        EMG_filter_basic.__init__(self, sample_frequency = sample_frequency, range_ = range_)
+    def __init__(self, sample_frequency = 200, range_ = 0.5, min_EMG_frequency = 25, max_EMG_frequency = 150, reference_available = False):
+        EMG_filter_basic.__init__(self, sample_frequency = sample_frequency, range_ = range_, reference_available = reference_available)
 
-        self.for_pkpk = []
+        #self.for_pkpk = []
 
-        self.neutral = -1                                                   #signal is shifted so that point point is zero
-        self.min_EMG_frequency = min_EMG_frequency                          #signals below this frequency do not influence the peak to peak measurements
-        self.max_EMG_frequency = max_EMG_frequency                          #signals above this frequency do not influence the peak to peak measurements
-        self.min_pk_gap = 1.0/self.max_EMG_frequency * self.sample_frequency  #the minimun distance in data points that two registered peaks can be 
-        self.max_pk_gap = 1.0/self.min_EMG_frequency * self.sample_frequency  #the maximum distance two consecutive peaks can be without the calculated neutral-point shifting significantly 
-        self.pk_indices = [] 
-        self.pk_is_convex = []
+        #self.neutral = -1                                                   #signal is shifted so that point point is zero
+        #self.min_EMG_frequency = min_EMG_frequency                          #signals below this frequency do not influence the peak to peak measurements
+        #self.max_EMG_frequency = max_EMG_frequency                          #signals above this frequency do not influence the peak to peak measurements
+        #self.min_pk_gap = 1.0/self.max_EMG_frequency * sample_frequency  #the minimun distance in data points that two registered peaks can be 
+        #self.max_pk_gap = 1.0/self.min_EMG_frequency * sample_frequency  #the maximum distance two consecutive peaks can be without the calculated neutral-point shifting significantly 
+        #self.pk_indices = [] 
+        #self.pk_is_convex = []
         #self.pkpk_stored = 10
 
+        self.reference_available = reference_available
+        self.PkPk = PkPk(sample_frequency = sample_frequency, min_frequency = min_EMG_frequency, max_frequency = max_EMG_frequency)
+
+    '''
     #returns dictionary of max, min, difference, and neutral values
     def get_pkpk(self, data):
         self.for_pkpk.insert(0, data)
@@ -89,21 +181,20 @@ class EMG_filter(EMG_filter_basic):
             return {'max' : -1, 'min' : -1, 'pkpk' : -1, 'neutral' : -1}
         else:
             return to_return
-
-
-    def find_peaks(self):
-        #cannot find another peak of oposite concavity until min_pk_gap data points past? 
-        pass
-
-    def advanced_get_pkpk(self):
-        pass
+    '''
 
     #this function is called to input raw data and return a filtered value, accounting for low-frequency noise and un-normalized data
     def filter(self, data):
+        '''
         pkpk = self.get_pkpk(data)
         #print pkpk
         return super(EMG_filter, self).filter(data - pkpk['neutral'])
         #return super(EMG_filter, self).filter(data)
+        '''
+
+        neutral_value = self.PkPk.get_pkpk(data)['neutral']
+        return super(EMG_filter, self).filter(data - neutral_value)
+
 
     def test_print(self):
         print "TEST PRINT 222222"
---
+++ Python/test1.py
@@ -0,0 +1,53 @@
+import csv_generator, emg
+from pprint import pprint
+
+#f = emg.EMG_filter_basic()
+#pprint (vars(f))
+#pprint (vars(f.f))
+
+'''
+
+m = emg.Moving_Average(8)
+
+pprint(vars(m))
+
+
+
+for i in range(100):
+	print m.get_movingAvg(i)
+
+print " "
+
+
+l = emg.LPF(0.125, 8)
+
+pprint(vars(l))
+
+for i in range(100):
+	print l.filter(i)
+
+print " "
+
+'''
+
+b = emg.EMG_filter(sample_frequency = 80, range_ = 0.1, reference_available = False)
+
+for i in range(100):
+	print b.filter(i)
+
+print " "
+
+pprint(vars(b))
+
+
+
+
+
+
+
+
+
+
+
+
+
