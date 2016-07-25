3bf498113f95f3da79200fc282518030ff38b9e0
Jeremy Malloch <jeremalloch@gmail.com>
2016-07-25 13:44:21 -0400

    Implemented an exponential moving average filter, as well as swapped all lists for deques so that the algorithms run in constant time

--- Python/emg.py
+++ Python/emg.py
@@ -1,9 +1,11 @@
 import signal_utilities as su
+from collections import deque
+
 
 #Simple filter class assumes no low-frequency noise and data centred at 0. More complex filteres inherit from this.
 class EMG_filter_basic(object):
     def __init__(self, sample_frequency = 200, range_ = 0.1, reference_available = False):
-        self.log = []                                          #used to store filtered data
+        self.log = deque([])   #used to store filtered data
         self.reference_available = reference_available
         self.MA = su.Moving_Average(length = sample_frequency * range_, return_int = True)
 
@@ -13,7 +15,7 @@ class EMG_filter_basic(object):
 
     #input filtered data
     def log_data(self, to_log):
-        self.log.insert(0, to_log)
+        self.log.appendleft(to_log)
 
     #pop and return either one data point or a set of data points
     def pop_log(self, num_to_pop = 1):
@@ -33,14 +35,14 @@ class EMG_filter_basic(object):
     #this function is called to input raw data. It returns a filtered value if it has enough samples, and also logs it
     def filter(self, data, reference_data = 0):
         if not self.reference_available:
-            filtered_value = self.MA.get_movingAvg(self.rectify(data))
+            filtered_value = self.MA.get_MA(self.rectify(data))
             self.log_data(filtered_value)
             return filtered_value
 
         else:
             clean_data = self.LPF_data.filter(data)
             clean_reference = self.LPF_reference.filter(reference_data)
-            filtered_value = self.MA.get_movingAvg(self.rectify(clean_data - clean_reference))
+            filtered_value = self.MA.get_MA(self.rectify(clean_data - clean_reference))
             self.log_data(filtered_value)
             #print "Reference used"
             return filtered_value
@@ -61,7 +63,7 @@ class EMG_filter(EMG_filter_basic):
             data = clean_data - clean_reference
         
         neutral_value = self.PkPk.get_pkpk(data)['neutral']
-        filtered_value = self.MA.get_movingAvg(self.rectify(data - neutral_value))
+        filtered_value = self.MA.get_MA(self.rectify(data - neutral_value))
         self.log_data(filtered_value)
         return filtered_value
 
--- Python/signal_utilities.py
+++ Python/signal_utilities.py
@@ -1,30 +1,28 @@
-#Encapsulation of a key function that some filters require
+from collections import deque
+
+
 class Moving_Average(object):
+    """
+    Encapsulation of a key function that some filters require
+    Note: MA stands for moving average
+    """
     def __init__(self, length, return_int = False):
-        self.data = []
+        self.data = deque([])
         self.data_sum = -1
-        #self.data_avg = -1
         self.length = length
         self.value = -1
         self.return_int = return_int
 
-        #self.sample_frequency = sample_frequency               #in Hz
-        #self.range_ = range_                                   #in seconds
-        #self.scope = 1.0 * self.sample_frequency * self.range_       #in number of samples, limits the length of movingAvg    
-        #self.sum_movingAvg = 0                                 #tracks the sum of the moving average
-        #self.val_movingAvg = -1                                #the latest moving average value
-        #self.movingAvg = []                                    #used to store the datapoints for taking a moving average
-
-    def get_movingAvg (self, data):
-        self.data.insert(0, data)
+    def get_MA (self, data):
+        self.data.appendleft(data)
         self.data_sum += data
 
         if len(self.data) > self.length:
             self.data_sum -= self.data.pop()
 
         if self.return_int == True:
-            self.value = int(self.data_sum / self.length) #preserves integer form
-        else: 
+            self.value = self.data_sum // self.length   #preserves integer form
+        else:
             self.value = 1.0 * self.data_sum / self.length
 
         if len(self.data) < (self.length / 2):
@@ -32,8 +30,58 @@ class Moving_Average(object):
         else:
             return self.value
 
-#Simplifies the creation of a moving average-based LPF 
+
+class ExpMovingAverage:
+    """
+    An exponential moving average filter with variable exponentiating factors.
+    Does not return an exponential moving average while the log is being populated at first (it would be too
+    computationally expensive calculate the sum for each step to the end of the frame in order to have the weighting
+    factors sum to 1)
+    Note: As above, MA stands for moving average, and EMA stands for Exponential Moving Average
+    """
+    def __init__(self, expFactor_ = 0.5, window_ = 50):
+        """
+        expFactor is what each term is multiplied by each time it is shift right in the current frame
+        window is the number of samples that are taken into account when calculating the EMA
+        0 < expFactor < 1
+        1 < window
+        """
+        if 0 < expFactor_ < 1:
+            self.expFactor= float(expFactor_)
+        else:
+            raise ValueError("expFactor must be between 0 and 1")
+        self.window = window_   # Number of samples that are factored into calculation
+        self.sumEMA = 0   # Initialize the sum of the EMA as zero
+        self.EMA = deque([], maxlen = self.window)  # Initialize an empty deque iterable to contain the EMA calculated
+        # for the past
+        self.log = deque([], maxlen = self.window)    # Initialize an empty deque iterable of the unfiltered datapoints
+        self.finalFactor = self.expFactor**self.window  # Precomputed weighting factor applied to the last entry in the
+        # EMA sum
+        self.weightFactor = sum([self.expFactor ** x for x in range(1, self.window)]) * 1.0 # Value sumEMA is divided
+        # by so that sum of EMA weights is normalized to 1
+
+    def get_EMA(self, dataPoint):
+        """
+        Calculates the exponential moving average for the most recent points
+        """
+        if len(self.log) < self.window - 1:
+            # If the log will not be as long as the full window after the new dataPoint is added, no EMA will be
+            # calculated or returned
+            self.sumEMA = self.expFactor * (dataPoint + self.sumEMA)
+            self.log.appendleft(dataPoint)
+            return
+        else:
+            self.sumEMA = ((self.sumEMA - self.finalFactor * self.log[-1]) + dataPoint) * self.expFactor
+            self.log.appendleft(dataPoint)
+            return_value = self.sumEMA / self.weightFactor
+            self.EMA.appendleft(return_value)
+            return return_value
+
+
 class LPF(Moving_Average):
+    """
+    Simplifies the creation of a moving average-based low pass filter
+    """
     def __init__(self, cutoff_frequency, sample_frequency, return_int = False):
         length = int(0.125 * sample_frequency / cutoff_frequency)
         Moving_Average.__init__(self, length, return_int = return_int)
@@ -42,18 +90,18 @@ class LPF(Moving_Average):
         if self.length < 2:
             return to_filter
         else:
-            return self.get_movingAvg(to_filter) 
+            return self.get_MA(to_filter)
 
 class Basic_Stats(object):
     def __init__(self, length):
         self.length = length
-        self.data_points = []
+        self.data_points = deque([])
         self.total_sum = -1
         self.average = -1
         self.stddev = -1
     
     def add_data(self, data):
-        self.data_points.insert(0, data)
+        self.data_points.appendleft(data)
         self.total_sum += data
 
         if len(self.data_points) > self.length:
@@ -66,8 +114,11 @@ class Basic_Stats(object):
 
 
 class PkPk(object):
+    """
+    Peak to peak filter
+    """
     def __init__(self, sample_frequency, min_frequency, max_frequency):
-        self.for_pkpk = []
+        self.for_pkpk = deque([])
         self.min_frequency = min_frequency
         self.max_frequency = max_frequency
         self.min_pk_gap = sample_frequency / max_frequency
@@ -79,11 +130,11 @@ class PkPk(object):
         #self.min_pk_gap = 1.0/self.max_EMG_frequency * sample_frequency     #the minimun distance in data points that two registered peaks can be 
         #self.max_pk_gap = 1.0/self.min_EMG_frequency * sample_frequency     #the maximum distance two consecutive peaks can be without the calculated neutral-point shifting significantly 
 
-        #self.pk_indices = [] 
-        #self.pk_is_convex = []
+        #self.pk_indices = deque([])
+        #self.pk_is_convex = deque([])
 
     def get_pkpk(self, data):
-        self.for_pkpk.insert(0, data)
+        self.for_pkpk.appendleft(data)
 
         #discards any data beyond two periods of the lowest-frequency wave
         if len(self.for_pkpk) > (self.max_pk_gap * 2): 
